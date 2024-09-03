c0ec9b0a9b50ac607b5186b8ef88336c1e608279
bharatyadav28 <yadavbharat386@gmail.com>
2024-09-03 15:00:26 +0530

    Fixed slot booking payment bug

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
+  baseURL: "https://ares-elite-backend.vercel.app/",
+  // baseURL: "http://localhost:5000"
+
   // baseURL: "https://ares-doctor.vercel.app",
-  baseURL: "http://localhost:5000"
   // baseURL: "https://ares-backend-main.onrender.com"
 });
 
