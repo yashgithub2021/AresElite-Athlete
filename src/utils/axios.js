fb37c7ce71cbb5c75952837dd28a1e8d17bc3790
Yash Barge <yashbarge007@gmail.com>
2024-09-09 10:38:41 +0530

    backend url change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://ares-elite-backend.vercel.app/",
-  baseURL: "http://localhost:5000"
+  baseURL: "https://ares-elite-backend.vercel.app/",
+  // baseURL: "http://localhost:5000"
 
   // baseURL: "https://ares-doctor.vercel.app",
   // baseURL: "https://ares-backend-main.onrender.com"
